b6cfa4c6cdafd980e665316063aee17183dfbc3a
YUDHISH SINGAPURI <singapuriyudhish27@gmail.com>
2026-05-18 13:16:37 +0530

    Normal Change

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -77,7 +77,8 @@ export default function Login() {
     <div className="max-w-md mx-auto py-12">
       <Card>
         <Card.Header>
-          <h1 className="text-2xl font-bold text-center">Login to NETBOND</h1>
+          <h1 className="text-2xl font-bold text-center">NETBOND</h1>
+          <h3 className="text-2xl font-bold text-center">Proceed To Login</h3>
         </Card.Header>
         
         <Card.Body>
